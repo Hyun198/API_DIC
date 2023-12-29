74f558bfcb2aef1da5b102e057002b55d2c6912f
Hyun <hwn123h@naver.com>
2023-12-29 23:27:01 +0900

    카드로 디자인

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
     .then(data => console.log(data.id))
     .catch(error => console.error(error)); */
 
-
+fetchData();
 async function fetchData() {
     try {
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        /*  const pokemonName = document.getElementById("pokemonName").value.toLowerCase(); */
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
 
         if (!response.ok) {
             throw new Error('cant fecth resource');
@@ -21,11 +21,29 @@ async function fetchData() {
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
+        const heightElement = document.getElementById("height");
+        const weightElement = document.getElementById("weight");
+        const NameElement = document.getElementById("name");
+        const typeElement = document.getElementById("type");
+
+        console.log(data.types[0].type.name);
+
+        const height = data.height;
+        const weight = data.weight;
+        const type = data.types[0].type.name;
+        const name = data.name;
+
+
 
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
 
-    } catch (error) {
+        heightElement.textContent = `Height: ${height}`;
+        weightElement.textContent = `Weight: ${weight}`;
+        NameElement.textContent = `${name}`;
+        typeElement.textContent = `${type}`;
 
+    } catch (error) {
+        console.error(error);
     }
 }
